9d91651c9c35d121b11ace0a8a403cede1dbbffc
smahuzaifa <huzaifasadath@gmail.com>
2025-11-13 10:44:04 +1100

    Chaining easy locator methods

--- tests/OtherLocators.spec.js
+++ tests/OtherLocators.spec.js
@@ -14,5 +14,13 @@ test('Other locators',async ({page})=>
         await page.getByPlaceholder("Password").fill("12345678");
         //GetByRole
         await page.getByRole("button",{name: 'Submit'}).click();
+        await page.getByRole("link",{name:'Shop'}).click();
+        await page.locator(".row").first().isVisible();
+        await page.getByRole("link",{name:'Nokia Edge'}).click();
+        await page.getByRole("link",{name:'Shop'}).click();
+        await page.locator(".row").first().isVisible();
+        await page.locator("app-card").filter({hasText:"Blackberry"}).getByRole("button").click();
+        //getByRole("button",{name:'Add '}) need not be added in full since there is only one in the chain 
+        //Another way to select when a locator returns multiple elements
         
 });
