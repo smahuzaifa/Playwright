a3a5cd1afc212437526fc92f54634cb3932f4100
smahuzaifa <huzaifasadath@gmail.com>
2025-11-16 19:47:34 +1100

    Added visible, hidden assertion and going back and forward in browser

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,16 @@
+const {test,expect} = require ('@playwright/test')
+test ("More validations",async ({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await page.goto("https://google.com");
+    await page.goBack(); //To go to the previous page or the back button on the browser
+    await page.goForward(); //The forward button on the browser
+    await page.goBack();
+    //Hide and Show assertion
+    const hideShowBox = page.locator("#displayed-text");
+    await expect(hideShowBox).toBeVisible(); //Asserts if the element is visible
+    await page.getByRole("button",{name:'Hide'}).click();
+    await expect(hideShowBox).toBeHidden(); //Asserts if the element is hidden
+
+
+});
